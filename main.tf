b76cafbcd2089543ccc6bca3a8dbdb2c0ad55121
Mirae KIM <mirae.kim@hec.edu>
2021-05-19 13:49:23 -0400

    all terraform

---
+++ main.tf
@@ -0,0 +1,128 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_security_group" "my-sg" {
+  name = "security_group"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # add specific IP here to whitelist
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "mirae_test" {
+  ami           = "ami-0b9064170e32bde34"
+  instance_type = "t2.micro"
+  iam_instance_profile = "${aws_iam_instance_profile.ec2_profile.name}"
+  key_name="doctolib_test" # must create your own key pair
+  vpc_security_group_ids = [aws_security_group.my-sg.id]
+  user_data = <<EOF
+#! /bin/bash
+/bin/echo "update apt gets"
+sudo apt update
+sudo apt-get update
+
+/bin/echo "get dcker"
+yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-cache policy docker-ce
+yes | sudo apt install docker-ce
+
+/bin/echo "get docker compose"
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+/bin/echo "update  mockserver docker"
+sudo docker pull mockserver/mockserver
+sudo docker run -d --rm -P mockserver/mockserver
+EOF
+}
+
+
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+      tag-key = "tag-value"
+  }
+}
+
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = "${aws_iam_role.ec2_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = "${aws_iam_role.ec2_role.name}"
+}
+
+
+
+
+resource "aws_s3_bucket" "mirae_bucket" {
+  bucket = "doctolib-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+
+
+
